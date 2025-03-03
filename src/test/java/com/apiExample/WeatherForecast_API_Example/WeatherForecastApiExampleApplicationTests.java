9b3ecbcbd47eab54265bd171500d0c4cd5f4a0e6
Kevin Åhlin <kevin.ahlin@hotmail.com>
2025-03-03 11:44:02 +0100

    Initial commit

---
+++ src/test/java/com/apiExample/WeatherForecast_API_Example/WeatherForecastApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiExample.WeatherForecast_API_Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
